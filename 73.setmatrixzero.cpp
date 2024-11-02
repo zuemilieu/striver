5cfded00ecfd7cdb64e8da0ddc588939a2a4c4e3
shruti kumari <keshrishruti177@gmail.com>
2024-11-02 21:19:36 +0530

    Create 73.setmatrixzero.cpp

---
+++ 73.setmatrixzero.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+class Solution{
+public:
+ void setZeros(vector<vector<int>>& matrix) {
+   int row = matrix.size();
+   int cols= matrix[0].size();
+   bool firstrowhaszero =false;
+   bool firstcolhaszero = false;
+   for
+ }
+
+};
