19c0491f00a2f7e6f7a110d1f2e6ebc013320982
shruti kumari <keshrishruti177@gmail.com>
2024-11-04 20:27:40 +0530

    Create max subarray.cpp

---
+++ max subarray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+ int maxSubarray(vector<int> & nums){
+   int sum =0;
+   int maxi = nums[0];
+   for(int i =0; i<nums.size(); i++)
+     {
+       sum = sum + nums[i];
+       maxi = max(sum, maxi);
+       if(sum <0)
+         sum =0;
+     }
+   return maxi;
+ }
+   
+
+ };
